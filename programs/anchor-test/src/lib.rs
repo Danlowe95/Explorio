30cb0b59e90507ca3bd2dd611510bce8ae1200a4
Daniel Lowe <danlowe95@gmail.com>
2021-12-22 17:21:05 -0500

    first commit

---
+++ programs/anchor-test/src/lib.rs
@@ -0,0 +1,50 @@
+use anchor_lang::prelude::*;
+
+declare_id!("ctqhWwv4TABZqzX3WJsJhiQDY6xNpVYW7KHtJL3ZRKC");
+
+#[program]
+mod anchor_test {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+    pub fn basic_test(ctx: Context<BasicTest>) -> ProgramResult {
+        Ok(())
+    }
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
+
+#[derive(Accounts)]
+pub struct BasicTest {
+}
---
+++ tests/anchor-test.ts
@@ -0,0 +1,47 @@
+import * as anchor from "@project-serum/anchor";
+import assert = require("assert");
+import { Program } from "@project-serum/anchor";
+import { AnchorTest } from "../target/types/anchor_test";
+
+describe("anchor-test", () => {
+  // Configure the client to use the local cluster.\
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.AnchorTest as Program<AnchorTest>;
+  const baseAccountLocal = anchor.web3.Keypair.generate();
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.rpc.basicTest({});
+    console.log("Your transaction signature", tx);
+  });
+  it("Creates a counter", async () => {
+    const tx = await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccountLocal.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [baseAccountLocal],
+    });
+    const baseAccountData = await program.account.baseAccount.fetch(
+      baseAccountLocal.publicKey
+    );
+    const newCount = baseAccountData.count;
+    console.log("newCount: " + newCount);
+    assert.ok(newCount.toString() == "0");
+    // _baseAccount = baseAccount;
+  });
+  it("increments", async () => {
+    // const base
+    const tx = await program.rpc.increment({
+      accounts: { baseAccount: baseAccountLocal.publicKey },
+      // signers: [baseAccountLocal],
+    });
+    const baseAccountData = await program.account.baseAccount.fetch(
+      baseAccountLocal.publicKey
+    );
+    assert.ok(baseAccountData.count.toString() == "1");
+  });
+});
